6b45b0af5b9276d086c7c33781de92db932df8e5
greinisch <greinisch@youwol.com>
2022-02-03 17:05:35 +0100

    🔧 yw_pipeline => use new API of py-youwol

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,8 +1,15 @@
-from youwol.configuration import Pipeline
-from youwol.configuration import UserConfiguration
+from youwol.environment.forward_declaration import YouwolEnvironment
+from youwol.environment.models import IPipelineFactory
+from youwol.environment.models_project import BrowserApp
+from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig
+from youwol_utils.context import Context
 
-def pipeline(configuration: UserConfiguration) -> Pipeline:
 
-    return configuration.get_custom_pipeline("typescript-webpack-npm")
+class PipelineFactory(IPipelineFactory):
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
+    async def get(self, env: YouwolEnvironment, context: Context):
+        config = PipelineConfig(target=BrowserApp())
+        return await pipeline(config, context)

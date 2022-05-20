e1a9ea84a84947d4c5a404ec43ce6f9bc1304d1d
greinisch <greinisch@youwol.com>
2022-05-20 18:13:58 +0200

    ♻️ main.ts => cdn.install cleanings

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,31 +1,26 @@
-export {};
-require("./style.css");
-let cdn = window["@youwol/cdn-client"];
+export {}
+require('./style.css')
+let cdn = window['@youwol/cdn-client']
 
 await cdn.install(
-  {
-    modules: [
-      "lodash",
-      "rxjs",
-      "@youwol/flux-core",
-      "@youwol/flux-view",
-      "@youwol/fv-group",
-      "@youwol/fv-button",
-      "@youwol/fv-tree",
-      "@youwol/fv-tabs",
-      "@youwol/fv-input",
-      "@youwol/fv-context-menu",
-      "@youwol/platform-essentials",
-    ],
-    css: [
-      "bootstrap#4.4.1~bootstrap.min.css",
-      "fontawesome#5.12.1~css/all.min.css",
-      "@youwol/fv-widgets#latest~dist/assets/styles/style.youwol.css",
-    ],
-  },
-  {
-    displayLoadingScreen: true,
-  }
-);
+    {
+        modules: [
+            'lodash',
+            'rxjs',
+            '@youwol/flux-view',
+            '@youwol/fv-group',
+            '@youwol/fv-tabs',
+            '@youwol/os-core',
+        ],
+        css: [
+            'bootstrap#4.4.1~bootstrap.min.css',
+            'fontawesome#5.12.1~css/all.min.css',
+            '@youwol/fv-widgets#latest~dist/assets/styles/style.youwol.css',
+        ],
+    },
+    {
+        displayLoadingScreen: true,
+    },
+)
 
-await import("./on-load");
+await import('./on-load')

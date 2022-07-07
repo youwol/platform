b658a12fbbaf8332565e7570b63b4961f07dd0d1
greinisch <greinisch@youwol.com>
2022-07-07 08:07:12 +0200

    ♻️ launch-pad badge => use new icons with bullets (colored for running app)

--- src/app/top-banner/top-banner.view.ts
+++ src/app/top-banner/top-banner.view.ts
@@ -28,14 +28,35 @@ class YouwolMenuView extends TopBannerMenuView {
         super({
             state,
             iconView: {
-                innerHTML: `
-<svg id="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 109.58 121.1">
-  <defs></defs>
-  <title>logo_YouWol_white</title>
-  <polygon class="fv-fill-primary" points="109.58 94.68 109.58 84.14 91.39 73.64 109.58 63.14 109.58 42.06 63.95 68.41 63.94 68.41 63.94 121.1 82.2 110.56 82.2 89.41 100.52 99.99 109.58 94.76 109.58 94.68"/>
-  <polygon class="fv-fill-primary" points="54.8 52.69 9.17 26.35 27.42 15.81 45.61 26.31 45.61 5.31 54.73 0.04 54.8 0 63.86 5.23 63.86 26.39 82.18 15.81 100.43 26.35 54.8 52.7 54.8 52.69"/>
-  <polygon class="fv-fill-primary" points="0.07 94.72 9.2 99.99 27.38 89.49 27.38 110.56 45.64 121.1 45.64 68.41 45.64 68.41 0.01 42.06 0.01 63.14 18.33 73.64 0 84.22 0 94.68 0.07 94.72"/>
-</svg>`,
+                class: 'd-flex flex-wrap',
+                style: {
+                    width: '30px',
+                },
+                children: children$(
+                    state.runningApplications$.pipe(
+                        map(
+                            (apps) =>
+                                new Set(apps.map((app) => app.cdnPackage)),
+                        ),
+                    ),
+                    (distinctApps) =>
+                        Array(9)
+                            .fill(null)
+                            .map((_, i) => {
+                                return {
+                                    class:
+                                        'border rounded ' +
+                                        (i < distinctApps.size
+                                            ? 'fv-bg-success'
+                                            : 'fv-bg-primary'),
+                                    style: {
+                                        width: '8px',
+                                        height: '8px',
+                                        margin: '1px',
+                                    },
+                                }
+                            }),
+                ),
             },
             contentView: () => new ApplicationsLaunchPadView({ state }),
         })

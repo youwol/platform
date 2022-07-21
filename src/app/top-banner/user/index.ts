df559b82391b03d4575f12492240b52360ea801d
greinisch <greinisch@youwol.com>
2022-07-21 17:02:53 +0200

    🐛 import ts-code-editor.view => should not be in exported in index (separated bundle)

--- src/app/top-banner/user/index.ts
+++ src/app/top-banner/user/index.ts
@@ -5,11 +5,4 @@ export * from './installers-tab.view'
 export * from './preferences-tab.view'
 export * from './profile-tab.view'
 export * from './settings-tabs'
-export {
-    CodeIdeState,
-    CodeIdeView,
-    TsCodeEditorView,
-    SrcHighlight,
-    SrcPosition,
-} from './ts-code-editor.view'
 export * from './vfs_default_map_cdn'

153e8239d887a184cd99fe9b176c6bc3787cd424
jehadmelad <jd.melad@gmail.com>
2023-10-26 13:07:26 +0200

    🔧  sync config files using pipeline ts

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -1,10 +1,10 @@
 
 const runTimeDependencies = {
     "externals": {
-        "marked": "^4.3.0",
+        "marked": "^4.2.3",
         "@youwol/fv-tabs": "^0.2.1",
         "@youwol/os-core": "^0.1.14",
-        "@youwol/fv-group": "^0.2.1",
+        "@youwol/fv-group": "^0.2.5",
         "@youwol/flux-view": "^1.0.3",
         "@youwol/http-clients": "^2.0.1",
         "rxjs": "^6.5.5",
